38250df737ad547d61ce9325ef595377e695b414
simongood <simongood2986@example.com>
2025-01-13 02:18:12 +0800

    retrieval v1.1.0 ; 主要更動 :  改動 qdrant_search object, 	原 : 要求config, 	更新後 : 要求config , 可指定要搜尋的 collection_name, 次要更動 : README.md 更新了使用說明

--- feature/retrieval/README.md
+++ feature/retrieval/README.md
@@ -10,4 +10,16 @@
 
 
 ## feature/retrival
-1. 向量搜索資料庫
+1. 向量搜索資料庫端
+- 使用方法 :
+
+```
+# 初始化物件
+qdrant_obj = qdrant_search(collection_name, config) # config 要求請看物件說明文件
+
+
+# 選擇方法
+result = qdrant_obj.cloud_search(input_query)   # for 情境搜尋
+result = qdrant_obj.trip_search(input_query)    # for 旅遊演算法
+
+```
--- feature/retrieval/qdrant_search.py
+++ feature/retrieval/qdrant_search.py
@@ -11,6 +11,7 @@ class qdrant_search:
     - input :  
 
         ```
+        collection_name = 想要搜尋的桶子名稱
         config = {
                     'jina_url':str, 
                     'jina_headers_Authorization':str,
@@ -31,20 +32,23 @@ class qdrant_search:
 
         ```
         .cloud_search( input_query: list[str] = ["形容客戶行程的一句話"] )
-        .trip_search( input_query: dict[list])
+        .trip_search( input_query: dict[list] = { "上午" : "形容客戶行程的一句話"})
         ```
     '''
     def __init__(
-        self,
-        config: dict = {
-                'jina_url':str, 
-                'jina_headers_Authorization':str,
-                'qdrant_url': str,
-                'qdrant_api_key': str
-            }
-        ):
+                self,
+                collection_name: str= 'test1',
+                config: dict = {
+                                'jina_url':str, 
+                                'jina_headers_Authorization':str,
+                                'qdrant_url': str,
+                                'qdrant_api_key': str
+                                }
+                ):
         
+        self.colleciton_name = collection_name
         self.config = config
+        
 
 
     def __search_query(self, input_query):
@@ -68,15 +72,16 @@ class qdrant_search:
         vector = embedding_data['embedding']   # dim = 1024
 
         # 2. 使用 vector 搜尋 qdrant 回傳 '相似度 > 某個分數' 的資料
-        qdrant_obj = qdrant_manager(collection_name='test1', 
+        qdrant_obj = qdrant_manager(collection_name=self.colleciton_name, 
                                     qdrant_url=config.get("qdrant_url"),
                                     qdrant_api_key= config.get("qdrant_api_key"))
-        result = qdrant_obj.search_vector(vector, 0.5)
+        result = qdrant_obj.search_vector(vector, 0.1)
         return result
 
 
     def cloud_search(self, input_query: list[str])-> list[dict]:
         '''
+        - 對情境搜尋
         - input :
 
             ```
@@ -98,6 +103,7 @@ class qdrant_search:
     
     def trip_search(self, input_query: dict)-> dict[list]: 
         '''
+        - 對旅遊演算法
         - input :
 
             ```
@@ -121,22 +127,10 @@ if __name__ == "__main__":
     if len(config) == 0:
         print('please check .env path')
 
-    qdrant_obj = qdrant_search(config)
+    qdrant_obj = qdrant_search('test1', config)
 
     # 情境搜索
     input_query = ['喜歡在文青咖啡廳裡享受幽靜且美麗的裝潢']
-    input_query = ['''
-        這家餐廳的氛圍溫暖宜人，精湛的烤功與復古風格交織，讓人一踏進來就感受到無比的舒適感。
-        進入這家餐廳，溫馨的環境搭配精緻的烤肉技術，營造出一種獨特的復古氛圍。
-        這家餐廳不僅提供美味的烤肉，還以其溫馨的裝潢和復古風格打破了傳統餐飲的格局。
-        這裡有著溫暖的氛圍，烤肉技術一流，並且每個細節都透露出復古的魅力。
-        餐廳內部充滿溫暖的氣息，烤製的美食口感十足，復古的風格設計也讓人如沐春風。
-        這家餐廳的設計讓人感到溫馨舒適，精湛的烤肉技術搭配上迷人的復古風格，為每位顧客帶來獨特的用餐體驗。
-        溫馨的餐廳氛圍搭配烤肉的香氣，復古風格的裝潢讓這家餐廳成為別具一格的美食天堂。
-        在這間餐廳中，您將感受到濃厚的溫暖氛圍，品嚐到極具特色的烤肉，並享受復古風格所帶來的懷舊感。
-        這是一家溫馨又充滿情調的餐廳，精緻的烤肉和復古風格的設計，營造出難以忘懷的用餐體驗。
-        這家餐廳給人一種親切的感覺，烤肉技術一流，復古的裝潢讓您回味無窮。
-                   ''']
     result = qdrant_obj.cloud_search(input_query)
     
     # 旅遊搜索
