d5fb4c2e052b650a9d74c3703f300812eba53daf
simongood <simongood2986@example.com>
2025-01-20 14:32:28 +0800

    Optimize classify_restaurant_or_view.py

--- feature/sql_v2/utils/classify_restaurant_or_view.py
+++ feature/sql_v2/utils/classify_restaurant_or_view.py
@@ -14,28 +14,27 @@ def classify_restaurant_or_view(placeID_list:list,
         list: 篩選後的 placeID 列表。
     ```
     '''
-    restaurant_types = ["小吃", "餐廳", "咖啡廳"]
-    view_types = ["一般商店", "日用品商店", "休閒設施", "伴手禮商店", "室內旅遊景點", "室外旅遊景點",
-                        "購物商場", "文化/歷史景點", "自然景點", "一般商店", "甜品店/飲料店"]
-        
-    filtered_df = ETL_dataframe.loc[placeID_list]
-    if classify == 'restaurant':
-        placeID_list_restaurant = (
-                        filtered_df[filtered_df['new_label_type'].isin(restaurant_types)]
-                        .index
-                        .tolist()
-        )
-        return placeID_list_restaurant
-    elif classify == 'view':
-        placeID_list_view = (
-                        filtered_df[filtered_df['new_label_type'].isin(view_types)]
-                        .index
-                        .tolist()
-        )
-        return placeID_list_view
-    else :
+    classify_map = {
+        'restaurant': ["小吃", "餐廳", "咖啡廳"],
+        'view': ["一般商店", "日用品商店", "休閒設施", "伴手禮商店", "室內旅遊景點", "室外旅遊景點",
+                            "購物商場", "文化/歷史景點", "自然景點", "一般商店", "甜品店/飲料店"]
+    }
+
+    # 確認類別是否有效
+    if classify not in classify_map:
         print('不篩選餐廳或景點')
         return []
+    
+    # 篩選邏輯
+    else:
+        filtered_series = ETL_dataframe['new_label_type'].loc[placeID_list]
+        filtered_list = (
+            filtered_series[filtered_series.isin(classify_map[classify])]
+            .index
+            .tolist()
+        )
+
+        return filtered_list
 
 
 if __name__ == '__main__':
