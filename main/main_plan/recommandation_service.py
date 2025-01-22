1988e262b0ec1c3dcfd41903d3e46b68857e8c49
LAPTOP-AJC7HJCN\Weiii <dannyliu155202@gmail.com>
2025-01-22 14:31:36 +0800

    fix the function from csv_read to sql_csv

--- main/main_plan/recommandation_service.py
+++ main/main_plan/recommandation_service.py
@@ -2,7 +2,7 @@ from feature.llm.LLM import LLM_Manager
 from feature.retrieval.qdrant_search import qdrant_search  # 修改這裡
 from feature.retrieval.utils import jina_embedding, json2txt, qdrant_control
 from feature.plan import CBRA
-from feature.sql import csv_read_2
+from feature.sql_csv import sql_csv
 
 def recommandation(user_Q, config):
     LLM_obj = LLM_Manager(config['ChatGPT_api_key']) # 初始化 LLM 物件
@@ -22,7 +22,9 @@ def recommandation(user_Q, config):
         limit=1000,
     )
     two = qdrant_obj.cloud_search(a) #透過llm的「形容客戶行程的一句話」，用向量資料庫去對比搜尋 
-    three = csv_read_2.pandas_search(two, b) #透過向量資料庫跟llm的用戶特殊要求去抓出前100名符合的
+    print(two)
+    three = sql_csv.pandas_search(system='plan',system_input=two,special_request_list= b) #透過向量資料庫跟llm的用戶特殊要求去抓出前100名符合的
+    print(three)
     four = CBRA.run_test(three, c) #透過結構搜尋跟庫基本要求資料再向下篩選出15個符合的
     five = LLM_obj.store_fun(four) #透過情境搜尋演算法篩出的15筆再用llm選出最佳的3筆資料
     return five
