af01c71c99bae696bb9015b2ee78a005558ab718
simongood <simongood2986@example.com>
2025-02-04 20:52:53 +0800

    complete trip_basic_rec_extract

---
+++ feature/llm/utils/extractor/trip_basic_req_extractor.py
@@ -0,0 +1,79 @@
+from feature.llm.utils.extractor.format_valid.format_valid import (
+    is_valid_24_hour_time, 
+    is_float, 
+    is_valid_mm_dd,
+)
+
+def trip_basic_req_extractor(basic_req:list[dict], debuger: bool=False):
+    
+    try:
+        extract_basic_req = [{
+            '出發時間': str(basic_req['出發時間']) if is_valid_24_hour_time(basic_req['出發時間']) else '09:00',
+            '結束時間': str(basic_req['結束時間']) if is_valid_24_hour_time(basic_req['結束時間']) else '21:00',
+            '出發地點': str(basic_req['出發地點']), # 只能確定是字串
+            '結束地點': str(basic_req['結束地點']), # 只能確定是字串
+            '交通方式': basic_req['交通方式'] if basic_req['交通方式'] in ['大眾運輸','開車','騎車','步行'] else '大眾運輸',
+            '可接受距離門檻(KM)': basic_req['可接受距離門檻(KM)'] if is_float(basic_req['可接受距離門檻(KM)']) else 30,
+            '早餐時間': str(basic_req['早餐時間']) if is_valid_24_hour_time(basic_req['早餐時間']) else 'none',
+            '中餐時間': str(basic_req['中餐時間']) if is_valid_24_hour_time(basic_req['中餐時間']) else '12:00',
+            '晚餐時間': str(basic_req['晚餐時間']) if is_valid_24_hour_time(basic_req['晚餐時間']) else '18:00',
+            '預算': basic_req['預算'] if is_float(basic_req['預算']) else 'none',
+            '出發日': str(basic_req['出發日']) if is_valid_mm_dd(basic_req['出發日']) else 'none',
+        }]
+    except:
+        # 預設值
+        extract_basic_req = [{
+            '出發時間': '09:00',
+            '結束時間': '21:00',
+            '出發地點': '台北車站',
+            '結束地點': 'none',
+            '交通方式': '大眾運輸',
+            '可接受距離門檻(KM)': 30,
+            '早餐時間': 'none',
+            '中餐時間': '12:00',
+            '晚餐時間': '18:00',
+            '預算': 'none',
+            '出發日': 'none'
+        }]
+
+
+    if debuger == True :
+        from pprint import pprint
+        pprint(extract_basic_req, sort_dicts=False)
+
+    return extract_basic_req
+
+
+if __name__ == "__main__":
+    basic_req = [{
+        '出發時間': '00:00',         # '00:00'|'none'
+        '結束時間': '21:00',         # '00:00'|'none'
+        '出發地點': '台北車站',       # str|'none'          !! 要求一定要小地標
+        '結束地點': 'none',          # str|'none'          !! 要求一定要小地標
+        '交通方式': '大眾運輸',       # "大眾運輸" | "開車" | "騎車" | "步行"  !!  如果沒給就給 '大眾運輸'
+        '可接受距離門檻(KM)': 30,     # int         !! 沒給就給 30
+        '早餐時間': 'none',          # '00:00'|'none'
+        '中餐時間': '12:00',         # '00:00'|'12:00'      !! 沒給則給 '12:00'
+        '晚餐時間': '18:00',         # '00:00'|'18:00'      !! 沒給則給 '18:00'
+        '預算': 'none',              #  int | "none"        !! 確保一定有 "none"
+        '出發日': 'none'             # "mm-dd" | "none"     !! 確保一定有 "none"
+    }]
+
+    trip_basic_req_extractor(basic_req, debuger=True)
+
+
+    # ----------------is_valid_24_hour_time 測試---------------------
+    # print(is_valid_24_hour_time("00:00"))  # True
+    # print(is_valid_24_hour_time("23:59"))  # True
+    # print(is_valid_24_hour_time("24:00"))  # True
+    # print(is_valid_24_hour_time("24:01"))  # False
+    # print(is_valid_24_hour_time("12:60"))  # False
+    # print(is_valid_24_hour_time("-01:30")) # False
+
+
+    # ----------------測試 mm-dd 日期格式-------------------------------------------
+    # print(is_valid_mm_dd("01-01"))  # True
+    # print(is_valid_mm_dd("12-31"))  # True
+    # print(is_valid_mm_dd("02-30"))  # False
+    # print(is_valid_mm_dd("13-01"))  # False
+    # print(is_valid_mm_dd("00-10"))  # False
